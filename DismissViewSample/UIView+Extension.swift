12b95ee907a91fc07941a52df3d81dfc07d4703d
zhangsihuai <727087311@qq.com>
2016-10-09 09:32:23 +0800

    添加了些东西

--- DismissViewSample/UIView+Extension.swift
+++ DismissViewSample/UIView+Extension.swift
@@ -10,17 +10,19 @@ import UIKit
 
 enum AnimatedStyle{
     case Default
+    case Drop
+    case Tremble
 }
 
 
 extension UIView{
-    private var SHeight: CGFloat{
+    fileprivate var SHeight: CGFloat{
         get{
             return self.bounds.height
         }
     }
     
-    private var SWidth: CGFloat{
+    fileprivate var SWidth: CGFloat{
         get{
             return self.bounds.width
         }
@@ -39,41 +41,32 @@ extension UIView{
     }
    
     
-    @objc private func defaultDismiss(){
-        
-        UIView.animate(withDuration: 0.5, animations: {
-            self.dismissView?
-                .alpha = 0
-        }) { (_) in
-            self.dismissView?.removeFromSuperview()
-        }
-    }
-    
     func dismiss(withDuration duration: TimeInterval, animations: @escaping () -> ()){
     UIView.animate(withDuration: duration, animations: animations) { (_) in
             self.dismissView?.removeFromSuperview()
         }
-        
     }
     
     func dismiss(){
         defaultDismiss()
     }
-    
-    func addDismissView(){
+    func addDismissView(withStyle style: AnimatedStyle){
         for view in self.subviews{
             if view.tag == 1{
                 return
             }
         }
-        let tagGes = UITapGestureRecognizer(target: self, action: #selector(self.defaultDismiss))
-        self.addGestureRecognizer(tagGes)
-        let dismissView = UIView(frame: CGRect(x: 0, y: 0, width: SWidth * 0.5, height: SHeight * 0.3))
-        dismissView.layer.cornerRadius = 0.1 * dismissView.bounds.width
-        dismissView.center = self.center
-        dismissView.backgroundColor = UIColor.white
-        dismissView.tag = 1
-        addSubview(dismissView)
+        addDismissView()
+        var tapGes: UITapGestureRecognizer = UITapGestureRecognizer()
+        switch style {
+        case .Default:
+            tapGes = UITapGestureRecognizer(target: self, action: #selector(self.defaultDismiss))
+        case .Drop:
+            tapGes = UITapGestureRecognizer(target: self, action: #selector(self.dropDismiss))
+        case .Tremble:
+            tapGes = UITapGestureRecognizer(target: self, action: #selector(self.trembleDismiss))
+        }
+        self.addGestureRecognizer(tapGes)
     }
     
     func addDismissView(color: UIColor, alpha: CGFloat){
@@ -83,47 +76,76 @@ extension UIView{
                 flag = true
             }
         }
-        if flag {
-            let tagGes = UITapGestureRecognizer(target: self, action: #selector(self.defaultDismiss))
-            self.addGestureRecognizer(tagGes)
-            let dismissView = UIView(frame: CGRect(x: 0, y: 0, width: SWidth * 0.5, height: SHeight * 0.3))
-            dismissView.layer.cornerRadius = 0.1 * dismissView.bounds.width
-            dismissView.center = self.center
-            dismissView.backgroundColor = color
-            dismissView.alpha = alpha
-            dismissView.tag = 1
-            addSubview(dismissView)
+        if !flag {
+            addDismissView()
+            addDismissView(withStyle: .Default)
         }else{
-            self.subviews.first?.backgroundColor = color
-            self.subviews.first?.alpha = alpha
+            dismissView?.backgroundColor = color
+            dismissView?.alpha = alpha
+        }
+    }
+}
+
+extension UIView{
+    fileprivate func addDismissView(){
+        let dismissView = UIView(frame: CGRect(x: 0, y: 0, width: SWidth * 0.5, height: SHeight * 0.3))
+        dismissView.layer.cornerRadius = 0.1 * dismissView.bounds.width
+        dismissView.center = self.center
+        dismissView.backgroundColor = UIColor.white
+        dismissView.tag = 1
+        addSubview(dismissView)
+    }
+    //动画
+    @objc fileprivate func defaultDismiss(){
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.dismissView?.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.dismissView?.alpha = 0
+        }) { (_) in
+            self.dismissView?.removeFromSuperview()
         }
     }
     
-//    func addDismissView(Animations: @escaping () -> ()) {
-//        self.animations = Animations
-//        let tagGes = UITapGestureRecognizer(target: self, action: #selector(self.dismiss))
-//        self.addGestureRecognizer(tagGes)
-//        let dismissView = UIView(frame: CGRect(x: 0, y: 0, width: SWidth * 0.5, height: SHeight * 0.3))
-//        dismissView.layer.cornerRadius = 0.1 * dismissView.bounds.width
-//        dismissView.center = self.center
-//    }
+    @objc fileprivate func dropDismiss(){
+        dismissView?.layer.position = dismissView!.frame.origin
+        dismissView?.layer.anchorPoint = CGPoint(x: 0, y: 0)
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            self.dismissView?.layer.transform = CATransform3DMakeRotation(CGFloat(M_PI_4) * 0.8, 0, 0, 1)
+        }) { (_) in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.dismissView?.layer.transform = CATransform3DMakeRotation(CGFloat(M_PI_4) * 0.5, 0, 0, 1)
+                }, completion: { (_) in
+                    UIView.animate(withDuration: 0.5, animations: {
+                        self.dismissView?.alpha = 0
+                        let ty = self.dismissView!.superview!.frame.maxY
+                        self.dismissView?.frame.origin.y = ty
+                        }, completion: { (_) in
+                            self.dismissView?.removeFromSuperview()
+                    })
+            })
+        }
+    }
     
-//    func addDismissViewAnimated(withDuration: TimeInterval, animations: (_ dismissView: UIView) -> ()){
-//        if self.subviews.count == 0{
-//            let tagGes = UITapGestureRecognizer(target: self, action: #selector(self.dismiss))
-//            self.addGestureRecognizer(tagGes)
-//            let dismissView = UIView(frame: CGRect(x: 0, y: 0, width: SWidth * 0.5, height: SHeight * 0.3))
-//            dismissView.layer.cornerRadius = 0.1 * dismissView.bounds.width
-//            dismissView.center = self.center
-//            animations(dismissView)
-//        }else{
-//            animations(self.subviews.first!)
-//        }
-//        
-//    }
+    @objc fileprivate func trembleDismiss(){
+        let angle = 3 / 180 * CGFloat(M_PI)
+        let anim = CAKeyframeAnimation(keyPath: "transform.rotation")
+        let fadeAnim = CABasicAnimation(keyPath: "opacity")
+        anim.values = [angle,-angle]
+        anim.autoreverses = true
+        anim.repeatCount = MAXFLOAT
+        anim.duration = 0.5
+        anim.delegate = self
+        fadeAnim.toValue = 0
+        fadeAnim.duration = 0.5
+        fadeAnim.delegate = self
+        dismissView?.layer.add(anim, forKey: "anim")
+        dismissView?.layer.add(fadeAnim, forKey: "fadeAnim")
+    }
+}
 
-    
-    
-    
-    
+
+extension UIView: CAAnimationDelegate{
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        dismissView?.removeFromSuperview()
+    }
 }
--- DismissViewSample/ViewController.swift
+++ DismissViewSample/ViewController.swift
@@ -14,29 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layoutIfNeeded()
-        testView.addDismissView()
-        print(testView.subviews)
-//        testView.dismiss(withDuration: 2.0) { (dismissView) in
-//            dismissView?.alpha = 0
-//            dismissView?.frame.size.height = 200
-//        }
-    }
-    
-    @IBAction func buttonClick(_ sender: AnyObject) {
-        testView.dismiss(withDuration: 2.0) {
-            self.testView.dismissView?.backgroundColor = UIColor.blue
-            self.testView.dismissView?.alpha = 0.5
-        }
-    }
-   
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addDismissView(withStyle: .Tremble)
     }
 
 
